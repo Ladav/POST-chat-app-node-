d3377643e0c3c18b992452584d37b6292dabdeec
Davinder Singh <davindersaini003@gmail.com>
2020-01-24 21:17:35 +0530

    mobile view updates

--- public/js/chat.js
+++ public/js/chat.js
@@ -129,22 +129,6 @@ $locBtn.addEventListener('click', () => {
     });
 });
 
-
-
-// // First we get the viewport height and we multiple it by 1% to get a value for a vh unit
-// let vh = window.innerHeight * 0.01;
-// // Then we set the value in the --vh custom property to the root of the document
-// document.documentElement.style.setProperty('--vh', `${vh}px`);
-
-// // We listen to the resize event
-// window.addEventListener('resize', () => {
-//     // We execute the same script as before
-//     let vh = window.innerHeight * 0.01;
-//     document.documentElement.style.setProperty('--vh', `${vh}px`);
-//   });
-
-
-
 // hide or show active user in mobile view
 $mobileActiveIcon.addEventListener('click', () => document.querySelector('.active__users').classList.toggle('mobile__user--active'));
 
